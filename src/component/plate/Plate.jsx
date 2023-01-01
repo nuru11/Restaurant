cfb95e697bdee909ee87aa5a83209056472955fe
nuru ibrahim <nuruibrahim11111@gmail.com>
2022-12-31 22:21:18 -0800

    line through

--- src/component/plate/Plate.jsx
+++ src/component/plate/Plate.jsx
@@ -8,8 +8,8 @@ const Plate = ({ p, handleClick }) => {
       <div className="plate-item-parent">
         <img className="plate-img" src={img} alt="" />
         <h3 className="plate-name">{name}</h3>
-        <p className="about-plate">old Price: {oldP}</p>
-        <h className="plate-price">new price: {newP}</h>
+        <p className="about-plate">old Price: $<span className='plate-price-span'>{oldP}</span></p>
+        <h className="plate-price">new price: ${newP}</h>
         <button onClick={() => handleClick(p)} className="buy-btn">
           take it
         </button>
